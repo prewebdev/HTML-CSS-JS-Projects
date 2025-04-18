3d1f39752c7530b41021fa878547e518ebd927bb
prema <178149897+prewebdev@users.noreply.github.com>
2025-04-17 22:30:16 -0700

    Add files via upload

---
+++ Link Shortener/script.js
@@ -0,0 +1,12 @@
+async function shortenURL() {
+    const url = document.getElementById("url").value;
+    const response = await fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
+    if(response.ok) {
+        const data = await response.text();
+        document.getElementById('result').innerHTML = `
+        shortened URL: <a href="${data}" target="_blank">${data}</a>
+        `;
+    } else {
+        document.getElementById('result').innerHTML = "Error shortening URL";
+    }
+}
